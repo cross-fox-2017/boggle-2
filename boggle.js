5a747b2c57269843d60a57825de52567319f5178
Syanmil <syanmil@yahoo.com>
2017-01-05 17:49:40 +0700

    initialize cekBoogle

---
+++ boggle.js
@@ -0,0 +1,13 @@
+// import BoogleBoard from "./boggle-board.js"
+
+var dummyData =
+[ [ 'C', 'G', 'S', 'I' ],
+  [ 'T', 'J', 'S', 'I' ],
+  [ 'N', 'M', 'E', 'E' ],
+  [ 'R', 'P', 'Y', 'A' ] ];
+
+function cekBoogle(data){
+  return data
+}
+
+console.log(cekBoogle("MESS"));
