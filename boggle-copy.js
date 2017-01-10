4e57f415d991d43e6c45a0953fcd37a3b5e885db
dgana <dhegana@gmail.com>
2017-01-10 07:55:18 +0700

    WIP 2

--- boggle-copy.js
+++ boggle-copy.js
@@ -17,3 +17,77 @@ for(let i = 0; i < board.length; i++){
 }
 
 console.log(grid)
+
+
+
+let arr = []
+    let tam = []
+    for(let i = 0; i < this.words.length; i++){
+      let arr2 = []
+      arr.push(arr2);
+      let x = 0
+      for(let j = 0; j < this.words[i].length; j++){
+        for(let k = 0; k < this.boggleBoard.length; k++){
+          for(let l = 0; l < this.boggleBoard[k].length; l++){
+            let arr3 = []
+            tam.push(arr3)
+            if(this.boggleBoard[k][l] === this.words[i][j]){
+              x++
+              tam[i].push(this.boggleBoard[k][l],k,l)
+              if(x === this.words[i].length){
+                for(let m = 0; m < tam.length; m++){
+                  arr[i].push({
+                    huruf: tam[m],
+                    x: tam[m],
+                    y: tam[m]
+                  })
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+    searchWord() {
+    let arr = []
+    for(let i = 0; i < this.words.length; i++){
+      let x = 0
+      for(let j = 0; j < this.words[i].length; j++){
+        for(let k = 0; k < this.boggleBoard.length; k++){
+          for(let l = 0; l < this.boggleBoard[k].length; l++){
+            if(this.boggleBoard[k][l] === this.words[i][j]){
+              x++
+              if(x === this.words[i].length){
+                arr.push(i);
+              } else {
+                return "Word Not Found!"
+              }
+            }
+          }
+        }
+      }
+    }
+    return console.log(arr)
+  }
+
+  // wordPosition() {
+  //   let arr = []
+  //   for(let i = 0; i < this.searchWord().length; i++){
+  //     for(let j = 0; j < this.words[this.searchWord()[i]].length; j++){
+  //       for(let k = 0; k < this.boggleBoard.length; k++){
+  //         for(let l = 0; l < this.boggleBoard[k].length; l++){
+  //           if(this.boggleBoard[k][l] === this.words[this.searchWord()[i]][j]){
+  //             arr.push({
+  //               huruf: this.boggleBoard[k][l],
+  //               x: k,
+  //               y: l
+  //             })
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return console.log(arr);
+  // }  
--- boggle.js
+++ boggle.js
@@ -6,19 +6,74 @@ class BoggleBoard {
 
   shake(x = 4, y = 4){
     let huruf = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let acak = shuffle(huruf)
     // huruf import dari data.js
-    let randomIndex = Math.floor(Math.random() * 26);
+    let increment = 0;
 
     // for limit ganti agar dinamis 4 * 4
     for(let i = 0; i < y; i++){
       let arr = [];
       this.boggleBoard.push(arr);
       for(let j = 0; j < x; j++){
-        this.boggleBoard[i].push(huruf[randomIndex]);
-        randomIndex = Math.floor(Math.random() * 26);
+        this.boggleBoard[i].push(huruf[increment]);
+        increment++
       }
     }
     return this.boggleBoard
+  }
+
+  position() {
+    let arr = []
+    for(let i = 0; i < this.boggleBoard.length; i++){
+      for(let j = 0; j < this.boggleBoard[i].length; j++){
+        arr.push({
+          huruf: this.boggleBoard[i][j],
+          x: i,
+          y: j
+        })
+      }
+    }
+    return arr
+  }
+
+  solve() {
+    let arr = []
+    for(let i = 0; i < this.words.length; i++){
+      let x = 0
+      for(let j = 0; j < this.words[i].length; j++){
+        for(let k = 0; k < this.boggleBoard.length; k++){
+          for(let l = 0; l < this.boggleBoard[k].length; l++){
+            if(this.boggleBoard[k][l] === this.words[i][j]){
+              x++
+              if(x === this.words[i].length){
+                arr.push(i);
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    let arr2 = []
+    for(let i = 0; i < arr.length; i++){
+      for(let j = 0; j < this.words[arr[i]].length; j++){
+        for(let k = 0; k < this.boggleBoard.length; k++){
+          for(let l = 0; l < this.boggleBoard[k].length; l++){
+            if(this.boggleBoard[k][l] === this.words[arr[i]][j]){
+              arr2.push({
+                huruf: this.boggleBoard[k][l],
+                x: k,
+                y: l,
+                i: arr[i]
+              })
+            }
+          }
+        }
+      }
+    }
+
+    console.log(arr2.length)
+    return console.log(arr2)
   }
 
   solve() {
@@ -35,8 +90,7 @@ class BoggleBoard {
   }
 
   data() {
-    // let words = ['AA','AB','AC','AD','EF','GH','IJ','KL','MN','OPQ','RST','UVW','XYZ'];
-    let words = ['AB']
+    let words = ['ABCD','EFGH','IJKL','MNOP','QRST','UVWX','YZ',"ABA","ABAD","ABADI","ABAH","ABAI","ABAL","ABANG","ABANGAN","ABDI","ABDIKASI","ABDOMEN","ABDOMINAL","DEKODE","DEKODER","DEKOLONISASI","DEKOMPOSER","DEKOMPOSISI","DEKOMPRESI","DEKONGESTAN","HIEROGLIF","HIFA","HIGIENE","HIGIENIS","HIGROGRAF","HIGROGRAM","HIGROMETER","HIGROMETRI","HIGROSKOP","KRONIS","KRONISME","KRONOBIOLOGI","KRONOGRAM","KRONOLOGI","KRONOLOGIS","SEREALIA","SEREALIN","SEREAT","SEREBRAL","SEREBROSPINAL","SEREBRUM","SEREGANG","SEREH"];
     return words
   }
 
@@ -45,7 +99,22 @@ class BoggleBoard {
   }
 }
 
+function shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
+
 let boggleBoard = new BoggleBoard();
 boggleBoard.shake();
-boggleBoard.print()
+boggleBoard.print();
+boggleBoard.position()
 boggleBoard.solve()
+// boggleBoard.solve()
